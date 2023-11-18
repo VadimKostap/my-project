f6310c7fe97f78e6310ff2c28648ed8586a4355e
Vadim <vadim.kostap@mail.ee>
2023-11-18 21:57:50 +0200

    added fibonacci number function

--- js/test_scripts.js
+++ js/test_scripts.js
@@ -111,3 +111,33 @@ function findMaxNumber(a, b ,c, d) {
 
 findMaxNumber(1, 5, 6.6, 10.5);
 findMaxNumber(1, 5, '6', '10');
+
+
+// fibonacci number function
+
+function fib(num) {
+    if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+        return "";
+    }
+
+    let result = '';
+    let first = 0;
+    let second = 1;
+
+    for (let i = 0; i < num; i++) {
+        if (i + 1 === num) {
+            result += `${first}`;
+            // Без пробела в конце
+        } else {
+            result += `${first} `;
+        }
+
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return result;
+}
+fib(13);
+console.log(fib(13));
